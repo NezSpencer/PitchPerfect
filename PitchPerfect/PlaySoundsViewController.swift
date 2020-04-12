16c1939529378f7770aacdd15b3b7a8a440b0e9d
Nnabueze Uhiara <nuhiara@opay.team>
2020-04-12 22:30:53 +0100

    Rename playSoundController buttons

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -12,18 +12,19 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
     
     var recordedAudioURL: URL!
-    @IBOutlet weak var slowButton: UIButton!
-    @IBOutlet weak var fastButton: UIButton!
-    @IBOutlet weak var highPitchButton: UIButton!
-    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
     @IBOutlet weak var echoButton: UIButton!
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
+    
     var audioFile:AVAudioFile!
     var audioEngine:AVAudioEngine!
     var audioPlayerNode: AVAudioPlayerNode!
     var stopTimer: Timer!
-
+    
     enum ButtonType: Int {
         case slow = 0, fast, chipmunk, vader, echo, reverb
     }
@@ -62,16 +63,4 @@ class PlaySoundsViewController: UIViewController {
         super.viewWillAppear(animated)
         configureUI(.notPlaying)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
